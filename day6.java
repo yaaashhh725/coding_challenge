69a54353e61ff73521ad730261e297dc7ce7a3f3
yaaashhh725 <163151090+yaaashhh725@users.noreply.github.com>
2024-11-17 23:49:12 +0530

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,34 @@
+//problem
+
+/*You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Examples:
+
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
+Input: arr = [4, 5, 2]
+Output: [2, 5, 4]
+Explanation: The elements of the array are 4 5 2. The reversed array will be 2 5 4.
+Input: arr = [1]
+Output: [1]
+Explanation: The array has only single element, hence the reversed array is same as the original.
+Constraints:
+1<=arr.size()<=105
+0<=arr[i]<=105
+*/
+//Solution:
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int front=0;
+        int back=arr.length - 1;
+        while(front<=back){
+            int temp = arr[front];
+            arr[front]=arr[back];
+            arr[back]=temp;
+            front++;
+            back--;
+        }
+    }
+}
