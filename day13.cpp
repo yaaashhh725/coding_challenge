d4d6d72e197a92b26f0bea01e92ece7a9a766293
yaaashhh725 <163151090+yaaashhh725@users.noreply.github.com>
2024-11-24 23:41:11 +0530

    Create day13.cpp

---
+++ day13.cpp
@@ -0,0 +1,37 @@
+//Problem:
+
+/*
+Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+Examples:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+Input: arr[] = [-2, -4]
+Output: -2
+Explanation: The subarray {-2} has the largest sum -2.
+Input: arr[] = [5, 4, 1, 7, 8]
+Output: 25
+Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+Constraints:
+1 ≤ arr.size() ≤ 105
+-109 ≤ arr[i] ≤ 104
+*/
+
+//Solution:
+
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        int curr_sum = arr[0];
+        int max_sum=arr[0];
+        for(int i=1;i<arr.size();i++){
+            curr_sum = max(arr[i],curr_sum+arr[i]);
+            max_sum = max(max_sum,curr_sum);
+        }
+        return max_sum;
+    }
+};
